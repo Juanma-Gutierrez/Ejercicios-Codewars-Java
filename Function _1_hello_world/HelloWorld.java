f3575d5213c23547fd9b56d2aa46c7c4a421c910
Juan Manuel Gutierrez <juanmanuelgutierrezmellado@gmail.com>
2022-11-18 16:54:09 +0100

    feat: HelloWorld

---
+++ Function _1_hello_world/HelloWorld.java
@@ -0,0 +1,100 @@
+/**
+ * greet.java
+ *
+ * @version: 18/11/2022
+ * @author: Juan Manuel Gutiérrez
+ *          https://github.com/Juanma-Gutierrez
+ */
+
+public class HelloWorld {
+    public static String greet() {
+        return "hello world!";
+    }
+}
+
+/********** OPCION 1 **********/
+/*
+ * public class HelloWorld {
+ * public static String greet() {
+ * return "hello world!";
+ * }
+ * }
+ */
+
+/********** OPCION 2 **********/
+
+/*
+ * public class HelloWorld {
+ * 
+ * public static boolean amShibe = false;
+ * 
+ * public static String greet() {
+ * 
+ * if(amShibe){
+ * String shibe =
+ * "                Y.                      _             \n" +
+ * "                YiL                   .```.           \n" +
+ * "   much world   Yii;                .; .;;`.          \n" +
+ * "                YY;ii._           .;`.;;;; :          \n" +
+ * " such hello     iiYYYYYYiiiii;;;;i` ;;::;;;;          \n" +
+ * "            _.;YYYYYYiiiiiiYYYii  .;;.   ;;;          \n" +
+ * "         .YYYYYYYYYYiiYYYYYYYYYYYYii;`  ;;;;          \n" +
+ * "       .YYYYYYY$$YYiiYY$$$$iiiYYYYYY;.ii;`..          \n" +
+ * "      :YYY$!.  TYiiYY$$$$$YYYYYYYiiYYYYiYYii.         \n" +
+ * "      Y$MM$:   :YYYYYY$! `` 4YYYYYiiiYYYYiiYY.        \n" +
+ * "   `. :MM$$b.,dYY$$Yii  :'   :YYYYllYiiYYYiYY         \n" +
+ * "_.._ :`4MM$!YYYYYYYYYii,.__.diii$$YYYYYYYYYYY         \n" +
+ * ".,._ $b`P`      4$$$$$iiiiiiii$$$$YY$$$$$$YiY;        \n" +
+ * "   `,.`$:       :$$$$$$$$$YYYYY$$$$$$$$$YYiiYYL       \n" +
+ * "     `;$$.    .;PPb$`.,.``T$$YY$$$$YYYYYYiiiYYU:      \n" +
+ * "    ;$P$;;: ;;;;i$y$ !Y$$$b;$$$Y$YY$$YYYiiiYYiYY      \n" +
+ * "    $Fi$$ .. ``:iii.`- :YYYYY$$YY$$$$$YYYiiYiYYY      \n" +
+ * "    :Y$$rb ````  `_..;;i;YYY$YY$$$$$$$YYYYYYYiYY:     \n" +
+ * "     :$$$$$i;;iiiiidYYYYYYYYYY$$$$$$YYYYYYYiiYYYY.    \n" +
+ * "      `$$$$$$$YYYYYYYYYYYYY$$$$$$YYYYYYYYiiiYYYYYY    \n" +
+ * "      .i!$$$$$$YYYYYYYYY$$$$$$YYY$$YYiiiiiiYYYYYYY    \n" +
+ * "     :YYiii$$$$$$$YYYYYYY$$$$YY$$$$YYiiiiiYYYYYYi'    ";
+ * 
+ * return shibe;
+ * }
+ * 
+ * return "hello world!";
+ * }
+ * }
+ */
+
+/********** OPCION 3 **********/
+
+/*
+ * public class HelloWorld
+ * {
+ * public static String greet()
+ * {
+ * String hw = "hello world!";
+ * return hw;
+ * }
+ * }
+ */
+
+/********** OPCION 4 **********/
+
+/*
+ * public class HelloWorld {
+ * // Write a public static function `greet` that returns "hello world!"
+ * public static String greet(){
+ * return "hello world!";
+ * }
+ * }
+ */
+
+/********** OPCION 5 **********/
+
+/*
+ * public class HelloWorld {
+ * public static String greet() {
+ * char[] chars={'\u0068', '\u0065','\u006c','\u006c','\u006f','\u0020',
+ * '\u0077','\u006f','\u0072','\u006c', '\u0064', '\u0021'};
+ * return new String(chars);
+ * }
+ * }
+ */
--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# Ejercicios-Codewars-Java
-Repositorio con soluciones a los ejercicios de la web Codewars realizados en Java
+Repositorio con ejercicios de la web Codewars realizados en Java
+
+# Ejercicios-Codewars-Java 🧰
+
+Repositorio con ejercicios de la web Codewars realizados en Java
+
+## Comenzando 🚀
+
+Cada ejercicio está probado y funciona correctamente sobre la plataforma Codewars a la fecha de publicación indicada en este Readme. Todos tienen el enunciado del ejercicio como fichero **README.md** donde puedes ver lo que se solicita.
+
+También se incluyen para cada ejercicio varias alternativas creadas por los usuarios.
+
+## Lista de ejercicios realizados:
+
+1. Nov 22 - Function 1-hello world
+
+
+## Lenguajes empleados:
+
+-   Java
+
+## Construido con 🛠️
+
+-   Visual Studio Code
+
+## Autor ✒️
+
+-   **Juanma Gutiérrez** - [Juanma-Gutierrez](https://github.com/Juanma-Gutierrez)
